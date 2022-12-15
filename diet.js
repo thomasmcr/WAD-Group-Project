76ea2ee7970c21c0d8ef7a5cf761765d83766aef
delma12 <119870594+delma12@users.noreply.github.com>
2022-12-15 10:57:15 +0000

    diet page add

---
+++ diet.js
@@ -0,0 +1,6 @@
+// Get the container element
+var container = document.getElementById("container");
+
+// Set the vertical and horizontal alignment
+container.style.verticalAlign = "middle";
+container.style.horizontalAlign = "center";
